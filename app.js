8eb984d37537885dfc1865458a94aac048259c1d
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-02-19 23:21:30 +0300

    fixed couple cascade ssues and added a js file

---
+++ app.js
@@ -0,0 +1,20 @@
+const footer = document.querySelector('footer')
+const share = document.querySelector('.share-icon');
+const avatar = document.querySelector('.avatar');
+const info = document.querySelector('.info');
+
+
+// share.addEventListener('click', ()=>{
+
+
+// footer.style.backgroundColor = '#48556a'
+// footer.style.borderRadius = '0 0 .5rem .5rem'
+
+ 
+  
+//   avatar.insertAdjacentHTML(
+//     `afterbegin`,
+//     ` <strong>SHARE</strong>`
+//   )
+
+// })
