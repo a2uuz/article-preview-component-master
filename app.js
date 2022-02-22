fe860ada031529f689511e85108ebc54e446811f
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-02-22 22:56:09 +0300

    made options for share bottom

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
-// const footer = document.querySelector('footer')
-// const icon = document.querySelector('.icon');
-// const info = document.querySelector('.info');
-// const share = document.querySelector('.share-icon');
-
-
-// share.addEventListener('click', ()=>{
-
-
-// footer.style.cssText = `
-// color: white;
-// background-color: #48556a;
-// border-radius: 0 0 .5rem .5rem;
-// `;
-
-// icon.style.cssText = `
-// background: none;
-// width: 35px;
-// border: none;
-// font-size: 1rem;
-// color: #8282a9;
-// `;
-
-// if (icon.length < 0) {
-//   icon.insertAdjacentHTML (
-//   `afterbegin`,
-//   `<h4>SHARE</h4>` 
-//   )}
-//   else{
-    
-//   }
-
-
-
-
-// info.style.visibility = 'hidden'
-
-// });
+    const shareOption = document.querySelector('.share-option');
+    let shareButton = document.querySelector('.share-button');
+    shareButton.addEventListener('click', function () {
+      this.classList.toggle('active');
+      shareOption.classList.toggle('active');
+    });
