ad03de9e05ff9ae81dc1db47a626d4c2ca4b0768
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-02-25 20:09:34 +0300

    minified css

--- app.js
+++
@@ -1,6 +0,0 @@
-    const shareOption = document.querySelector('.share-option');
-    let shareButton = document.querySelector('.share-button');
-    shareButton.addEventListener('click', function () {
-      this.classList.toggle('active');
-      shareOption.classList.toggle('active');
-    });
