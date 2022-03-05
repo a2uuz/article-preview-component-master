5eef05ed3de4ae421a0f0b3dc9598400c4a4db72
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-03-05 18:14:45 +0300

    fixed js shareButton typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-    const shareOption = document.querySelector('.share-option');
-    let shareButton = document.querySelector('svg');
+    const shareOption = document.querySelector('.options');
+    let shareButton = document.querySelector('.share-button');
     shareButton.addEventListener('click', function () {
       this.classList.toggle('active');
       shareOption.classList.toggle('active');
